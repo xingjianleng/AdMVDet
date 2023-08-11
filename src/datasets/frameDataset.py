5924d37497e9847207b1d7bdbfc2a403539701b9
Xingjian Leng <xingjianleng@gmail.com>
2023-08-11 14:42:24 +1000

    BUG FIX: mask out configs for padding (Sync from main)

--- src/datasets/frameDataset.py
+++ src/datasets/frameDataset.py
@@ -316,6 +316,16 @@ class frameDataset(VisionDataset):
             plt.imshow(img0)
             plt.show()
 
+        if self.interactive and step_counter is not None:
+            imgs = {step_counter: imgs[step_counter]}
+            configs = np.array([config for cam, config in configs.items()], dtype=np.float32)
+            # IMPORTANT: mask out padded items
+            padding_mask = np.arange(self.num_cam) > step_counter
+            configs[padding_mask] = -3
+        else:
+            step_counter = self.num_cam
+            configs = np.zeros([self.num_cam, 0])
+
         aug_imgs, aug_imgs_gt, aug_mats, aug_masks = [], [], [], []
         for cam, img in imgs.items():
             cam_img_bboxs, cam_img_pids = img_bboxs[cam], img_pids[cam]
